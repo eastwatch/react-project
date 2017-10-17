68c4ad4b0a26f3dc0d6923a633534729014720a6
We Are Almond <alex@wearealmond.com>
2017-10-18 00:47:51 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there' });
+});
+
+// dynamic port binding
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+// specify a start strict
